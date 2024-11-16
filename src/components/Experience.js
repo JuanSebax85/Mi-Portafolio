66e67fec10d96be058c7c4d9f2299d67cbb7bffd
JuanSebax85 <jsbarragana1@ucentral.edu.co>
2024-11-15 22:09:23 -0500

    Pagina de experiencia 15 de noviembre

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Experience.css';
 import { Helmet } from 'react-helmet';
 
 const Experience = () => {
@@ -7,13 +8,66 @@ const Experience = () => {
       <Helmet>
         <title>Experiencia</title>
       </Helmet>
-      <h2>Mi Experiencia</h2>
+      <h2>Experiencia</h2>
       <br></br>
-      <ul>
-        <li>Desarrollador Junior en XYZ Company</li>
-        <li>Freelance en proyectos de desarrollo web</li>
+      <ul className='ListaExperiencia'>
+        <li>Freelance en diseño y edición de imagenes 2018-2022</li>
+        <img
+          src="/Photoshop.png"
+          alt="Photoshop"
+          className="img-fluid rounded col-1 col-md-1 m-1"
+          draggable="false" />
+        <img
+          src="/Illustrator.png"
+          alt="Illustrator"
+          className="img-fluid rounded col-1 col-md-1 m-1"
+          draggable="false" />
+        <li>Freelance en edición de videos 2018-2022</li>
+        <img
+          src="/PremierePro.png"
+          alt="PremierePro"
+          className="img-fluid rounded col-1 col-md-1 m-1"
+          draggable="false" />
+        <li>Desarrollador Junior en RGC Activa 2023-2024</li>
+        <img
+          src="/JS.png"
+          alt="Javascript"
+          className="img-fluid rounded col-1 col-md-1 m-1"
+          draggable="false" />
+
+        <img
+          src="/CSharp.png"
+          alt="CSharp"
+          className="img-fluid rounded col-1 col-md-1 m-1"
+          draggable="false" />
+
+        <img
+          src="/react.png"
+          alt="React"
+          className="img-fluid rounded col-1 col-md-1 m-1"
+          draggable="false" />
+
+        <img
+          src="/SQL.png"
+          alt="SQL"
+          className="img-fluid rounded col-1 col-md-1 m-1"
+          draggable="false" />
       </ul>
+      
     </div>
+    <div className="container">
+  <div className="row justify-content-center">
+    <div className="col-12 col-md-8 col-lg-6">
+      <h2 className="text-center HojaVida">Hoja de vida</h2>
+      <embed
+        src="/HV Juan Sebastian Barragan.pdf"
+        type="application/pdf"
+        className="w-100"
+        style={{ height: '80vh' }}
+      />
+    </div>
+  </div>
+</div>
     <div>
         <footer class="bg-dark text-white text-center py-3 mt-5">
           <p>Desarrollado por Juan Sebastián Barragan</p>
